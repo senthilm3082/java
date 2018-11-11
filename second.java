360d245aff911b5f66815aedb88720e1c6429608
Kumar <senthilm777@yahoo.com>
2018-11-11 20:10:28 +0530

    java proj

---
+++ second.java
@@ -0,0 +1,10 @@
+public class first {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("This is my first Java Program"); // prints Hello World
+   }
+}
